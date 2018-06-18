822f191da9ef7774b6106c2714c4b990a5f27c7b
Fredius Tout Court <frediustcDev@gmail.com>
2018-06-18 22:46:47 +0000

    can send mail with attachement

--- php/scripts/createNews.php
+++ php/scripts/createNews.php
@@ -1,4 +1,11 @@
 <?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
 if (isset($_POST['createNews'])) {
 
     $sujet = clear($_POST['sujet']);
@@ -8,7 +15,7 @@ if (isset($_POST['createNews'])) {
     $img = $_FILES["img"];
     $ok = true;
 
-    if (isset($_FILES)) {
+    if (isset($_FILES) && !empty($_FILES)) {
 
         // file check
         $target_dir = $ind . "assets/images/";
@@ -59,16 +66,56 @@ if (isset($_POST['createNews'])) {
 
     if ($ok) {
         $date = new DateTime();
-        echo $date->getTimestamp();
 
         $n = 'Article_' . $date->getTimestamp() . '.jpg';
         $dest = $target_dir . $n;
-        if (move_uploaded_file($img["tmp_name"], $dest)) {
+        // if (move_uploaded_file($img["tmp_name"], $dest)) {
+
+        //send the email
+        $mail = new PHPMailer(true); // Passing `true` enables exceptions
+        try {
+            //Server settings
+            // $mail->SMTPDebug = 1; // Enable verbose debug output
+            $mail->isSMTP(); // Set mailer to use SMTP
+            $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true; // Enable SMTP authentication
+            $mail->Username = 'frediustc@gmail.com'; // SMTP username
+            $mail->Password = 'resurection'; // SMTP password
+            $mail->SMTPSecure = 'ssl'; // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 465; // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('frediustc@gmail.com', 'Batir SA');
+
+            foreach ($clients as $client) {
+                $mail->addAddress($client);
+            }
+            // $mail->addAddress('joe@example.net', 'Joe User'); // Add a recipient
+            // $mail->addAddress('ellen@example.com'); // Name is optional
+            // $mail->addReplyTo('info@example.com', 'Information');
+            // $mail->addCC('cc@example.com');
+            // $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz'); // Add attachments
+            $mail->addAttachment($img['tmp_name']); // Optional name
+
+            //Content
+            $mail->isHTML(true); // Set email format to HTML
+            $mail->Subject = $sujet;
+            $mail->Body = '<img src="' . $dest . '"alt="preview"/>This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = $corps;
+
+            $mail->send();
             alert('success: Newsletter envoyee!', 'success');
 
-        } else {
-            alert('erreur: Image ajoute!');
+        } catch (Exception $e) {
+            alert('Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
         }
+
+        // } else {
+        //     alert('erreur: Image ajoute!');
+        // }
 
     }
 
